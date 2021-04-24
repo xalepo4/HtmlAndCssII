fdc1db2d6da616cf9f3196312e43c5c2c587c478
Xavi Ledesma <xalepo4@gmail.com>
2021-04-24 10:21:09 +0200

    Add PEC2 scheleton with boostrap

---
+++ PEC2/src/assets/scripts/main.js
@@ -0,0 +1,17 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+import * as bootstrap from 'bootstrap'
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
