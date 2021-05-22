35bb37e399e743f0774bf0bb6a0d5e1096e1fa8c
Xavi Ledesma <xalepo4@gmail.com>
2021-05-22 16:15:24 +0200

    Implement members with tailwind and remove unused scss files

--- PEC3/tailwind.config.js
+++ PEC3/tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    textColor: {
+      'primary': '#7e5c64',
+      'secondary': '#ffffff'
+    },
+    flex: {
+      '4': '1 0 21%'
+    },
     extend: {},
   },
   variants: {
